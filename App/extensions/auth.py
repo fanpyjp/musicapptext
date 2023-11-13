cba08f86c913c943ef2715832b04abdf2722d5e8
Administrator <F1301882607@163.com>
2023-11-13 21:15:40 +0800

    第二次提交

--- App/extensions/auth.py
+++ App/extensions/auth.py
@@ -1,3 +1,4 @@
+import redis
 import jwt
 import datetime
 from jwt import exceptions
@@ -7,6 +8,11 @@ from django.http import HttpResponse, JsonResponse
 
 SALT = settings.SECRET_KEY
 
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
+
+redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
 
 def create_token(payload, timeout=20):
 
@@ -14,8 +20,12 @@ def create_token(payload, timeout=20):
         'typ': 'jwt',
         'alg': 'HS256'
     }
+    user_id = payload['userid']
+    struser_id = f'userid:{user_id}'
     payload['exp'] = datetime.datetime.utcnow() + datetime.timedelta(minutes=timeout)
     token = jwt.encode(payload=payload, key=SALT, algorithm="HS256", headers=headers)
+    redis_ms = redis_client.setex(name=struser_id, time=timeout * 60, value=token)
+    print(redis_ms)
     return token
 
 class JwtQueryParamMiddleware(MiddlewareMixin):
@@ -27,6 +37,7 @@ class JwtQueryParamMiddleware(MiddlewareMixin):
         try:
             verified_payload = jwt.decode(token, SALT,algorithms=['HS256'])
             request.jwt_payload = verified_payload
+            user_id = verified_payload.get('userid')
         except exceptions.ExpiredSignatureError:
             return JsonResponse({'error':'token已失效'})
             raise
@@ -35,5 +46,12 @@ class JwtQueryParamMiddleware(MiddlewareMixin):
             raise
         except jwt.InvalidTokenError:
             return JsonResponse({'error':'非法的token'})
+            raise
+        redis_key = f"userid:{user_id}"
+        print(redis_key)
+        redis_token = redis_client.get(redis_key)
+        print(redis_token)
+        if not redis_token or redis_token.decode('utf-8') != token:
+            return JsonResponse({'error':'token认证失败'})
             raise
         return None
--- musicapp/settings.py
+++ musicapp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf import settings
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,3 +130,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
